a1e79067a6bf53a0d63bcc7ba2df346dba5e78f0
adorechic <adorechic@gmail.com>
2016-11-25 23:12:17 +0900

    Extract method

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ extern crate iron;
 use iron::prelude::*;
 use iron::status;
 
+fn handler(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello")))
+}
+
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello")))
-    }).http("localhost:3000").unwrap();
+    Iron::new(handler).http("localhost:3000").unwrap();
 }
