24c5acdfbac0dce49e2d6713ddd6545c0088fdac
adorechic <adorechic@gmail.com>
2016-11-26 22:01:48 +0900

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 extern crate iron;
+extern crate logger;
+extern crate env_logger;
 
 use iron::prelude::*;
 use iron::status;
+use logger::Logger;
 
 fn handler(_: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Hello")))
 }
 
 fn main() {
-    Iron::new(handler).http("localhost:3000").unwrap();
+    env_logger::init().unwrap();
+    let (logger_before, logger_after) = Logger::new(None);
+    let mut chain = Chain::new(handler);
+    chain.link_before(logger_before);
+    chain.link_after(logger_after);
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
