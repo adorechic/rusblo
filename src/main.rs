44604b0b059ffa91897fb9fca27b78b38d9ca9b2
adorechic <adorechic@gmail.com>
2016-12-08 19:58:53 +0900

    Use dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+extern crate dotenv;
 extern crate env_logger;
 extern crate rusblo;
 
+use dotenv::dotenv;
 use std::env;
 use rusblo::model::migrate;
 use rusblo::controller::start_server;
 
 fn main() {
-    if let Err(_) = env::var("RUST_LOG") {
-        env::set_var("RUST_LOG", "info");
-    }
+    dotenv().ok();
     env_logger::init().unwrap();
 
     let args: Vec<String> = env::args().collect();
